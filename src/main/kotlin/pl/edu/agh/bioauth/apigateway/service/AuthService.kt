27243369ae3f6e760fe13459ae5c2365ceae7a6c
Julia Samól <julia.samol@gmail.com>
2019-04-27 17:28:27 +0200

    Save biometric pattern in the database

--- src/main/kotlin/pl/edu/agh/bioauth/apigateway/service/AuthService.kt
+++ src/main/kotlin/pl/edu/agh/bioauth/apigateway/service/AuthService.kt
@@ -1,10 +1,16 @@
 package pl.edu.agh.bioauth.apigateway.service
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.mongodb.gridfs.GridFsTemplate
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
+import pl.edu.agh.bioauth.apigateway.model.database.BiometricPattern
 import pl.edu.agh.bioauth.apigateway.model.network.RegisterResponse
 import pl.edu.agh.bioauth.apigateway.repository.AppRepository
+import pl.edu.agh.bioauth.apigateway.repository.BiometricPatternRepository
+import pl.edu.agh.bioauth.apigateway.util.KeyGenerator
+import pl.edu.agh.bioauth.apigateway.util.stringValue
+import pl.edu.agh.bioauth.apigateway.util.toFile
 
 @Service
 class AuthService {
@@ -12,7 +18,21 @@ class AuthService {
     @Autowired
     private lateinit var appRepository: AppRepository
 
+    @Autowired
+    private lateinit var biometricPatternRepository: BiometricPatternRepository
+
+    @Autowired
+    private lateinit var gridFsTemplate: GridFsTemplate
+
     fun registerPattern(samples: List<MultipartFile>, appId: String, appSecret: String, userId: String): RegisterResponse {
+        appRepository.findByAppIdAndAppSecret(appId, appSecret)?.let { app ->
+            val fileIds = samples.map(MultipartFile::toFile).map { gridFsTemplate.store(it.inputStream(), it.name) }
+            val keyPair = KeyGenerator.getKeyPair()
+            biometricPatternRepository.save(BiometricPattern(fileIds, app._id, userId, keyPair.private.stringValue))
+
+            return RegisterResponse(keyPair.public.stringValue)
+        }
+
         return RegisterResponse("")
     }
 }
---
+++ src/main/kotlin/pl/edu/agh/bioauth/apigateway/util/Extensions.kt
@@ -0,0 +1,28 @@
+package pl.edu.agh.bioauth.apigateway.util
+
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
+import java.security.KeyFactory
+import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
+import java.util.*
+
+fun MultipartFile.toFile(): File = File(System.getProperty("java.io.tmpdir"), originalFilename).also { transferTo(it) }
+
+val PrivateKey.stringValue: String
+    get() {
+        val keyFactory = KeyFactory.getInstance(KeyGenerator.KEY_ALGORITHM)
+        val keySpec = keyFactory.getKeySpec(this, PKCS8EncodedKeySpec::class.java)
+
+        return Base64.getEncoder().encodeToString(keySpec.encoded)
+    }
+
+val PublicKey.stringValue: String
+    get() {
+        val keyFactory = KeyFactory.getInstance(KeyGenerator.KEY_ALGORITHM)
+        val keySpec = keyFactory.getKeySpec(this, X509EncodedKeySpec::class.java)
+
+        return Base64.getEncoder().encodeToString(keySpec.encoded)
+    }
