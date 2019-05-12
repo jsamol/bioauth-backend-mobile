bdcf881ca3861033badd2fd508e0f6aed3f1dceb
Julia Samól <julia.samol@gmail.com>
2019-05-13 01:02:10 +0200

    Add json dependency

--- src/main/kotlin/pl/edu/agh/bioauth/apigateway/service/common/MetadataService.kt
+++ src/main/kotlin/pl/edu/agh/bioauth/apigateway/service/common/MetadataService.kt
@@ -1,6 +1,6 @@
 package pl.edu.agh.bioauth.apigateway.service.common
 
-import org.springframework.boot.configurationprocessor.json.JSONObject
+import org.json.JSONObject
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
